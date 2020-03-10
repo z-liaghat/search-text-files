7029d99753d63d68088cb5b84d4f24957ecb9a32
abbas <s.shafie.s@gmail.com>
2020-03-10 14:50:34 -0700

    Change Query Generator.

--- src/QueryGenerator.java
+++ src/QueryGenerator.java
@@ -1,7 +1,6 @@
 public class QueryGenerator {
 
     public String convertQueryToRegex(String query) {
-       query = replaceMetaCharacter(query);
         StringBuilder regexQuery = new StringBuilder();
         String[] operandBetweenORList;
         if (query.contains(Operators.OR.name())) {
@@ -25,23 +24,10 @@ public class QueryGenerator {
         String[] operand = andExpression.split(Operators.AND.name());
         StringBuilder regex = new StringBuilder();
         for (int j = 0; j < operand.length; j++) {
-//            regex.append("(?=.*\\Q" + operand[j].toLowerCase().trim() + "\\E)");
             regex.append("(?=.*" + operand[j].toLowerCase().trim() + ")");
         }
         regex.append(".*");
         return regex;
     }
 
-    private String replaceMetaCharacter(String expression) {
-        String newExpression = "";
-        //      ([{\^-=$!|]})?*+.
-        char[] metaChar = {'+', '*', '?', '|' ,'^','.','[',']','(',')','-','=','!','$','\\','{','}'};
-        for (int i = 0; i < metaChar.length; i++)
-            if (expression.indexOf(metaChar[i]) != -1) {
-               expression = expression.replace(metaChar[i] + "", "\\" + metaChar[i]);
-            }
-
-
-        return expression;
-    }
 }
